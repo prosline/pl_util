3c3647785c42ae1d2ed6e35be30518cf2017825a
marciodasilva <marciodasilva@hotmail.com>
2020-02-13 12:38:34 -0800

    Refactoring Rest Error API

--- utils/rest_errors/rest_error.go
+++ utils/rest_errors/rest_error.go
@@ -3,67 +3,98 @@ package rest_errors
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 )
 
-type RestErr struct {
-	Message string `json:"message"`
-	Status  int    `json:"status"`
-	Error   string `json:"error"`
-	Causes []interface{} `json"causes"`
+type restErr struct {
+	message string `json:"message"`
+	status  int    `json:"status"`
+	error   string `json:"error"`
+	causes []interface{} `json"causes"`
+}
+type RestErr interface {
+	Message() string
+	Status()  int
+	Error()   string
+	Causes() []interface{}
+
 }
 
-func NewBadRequestError(message string) *RestErr {
-	return &RestErr{
-		Message: message,
-		Status:  http.StatusBadRequest,
-		Error:   "bad_request",
+func (e restErr) Error() string{
+	return fmt.Sprintf("message: %s - status: %d - error: %s - causes: [%v]",
+		e.message,e.status,e.error,e.causes)
+}
+func (e restErr) Message() string{
+	return e.message
+}
+
+func (e restErr) Status() int{
+	return e.status
+}
+func (e restErr) Causes() []interface{}{
+	return e.causes
+}
+
+func NewRestError(message string, status int, err string, causes []interface{}) RestErr {
+	return restErr{
+		message: message,
+		status:  status,
+		error:   err,
+		causes:  causes,
 	}
 }
-func BadRequestError(message string) *RestErr {
-	return &RestErr{
-		Message: message,
-		Status:  http.StatusBadRequest,
-		Error:   "bad_request",
+func NewBadRequestError(message string) RestErr {
+	return restErr{
+		message: message,
+		status:  http.StatusBadRequest,
+		error:   "bad_request",
 	}
 }
-func CreateUserError(message string) *RestErr {
-	return &RestErr{
-		Message: message,
-		Status:  http.StatusInternalServerError,
-		Error:   "user_not_created",
+func BadRequestError(message string) RestErr {
+	return restErr{
+		message: message,
+		status:  http.StatusBadRequest,
+		error:   "bad_request",
 	}
 }
-func UserNotFound(message string) *RestErr {
-	return &RestErr{
-		Message: message,
-		Status:  http.StatusNotFound,
-		Error:   "not_found",
+func CreateUserError(message string) RestErr {
+	return restErr{
+		message: message,
+		status:  http.StatusInternalServerError,
+		error:   "user_not_created",
 	}
 }
-func NewUserBadRequest(message string) *RestErr {
-	return &RestErr{
-		Message: message,
-		Status:  http.StatusBadRequest,
-		Error:   "user_already_exists",
+func UserNotFound(message string) RestErr {
+	return restErr{
+		message: message,
+		status:  http.StatusNotFound,
+		error:   "not_found",
 	}
 }
-func NewInternalServerError(message string,err error) *RestErr {
-	ret := &RestErr{
-		Message: message,
-		Status:  http.StatusInternalServerError,
-		Error:   "internal_server_error",
+func NewUserBadRequest(message string) RestErr {
+	return restErr{
+		message: message,
+		status:  http.StatusBadRequest,
+		error:   "user_already_exists",
+	}
+}
+func NewInternalServerError(message string,err error) RestErr {
+	ret := restErr{
+		message: message,
+		status:  http.StatusInternalServerError,
+		error:   "internal_server_error",
 	}
 	if err != nil {
-		ret.Causes = append(ret.Causes, err.Error())
+		ret.causes = append(ret.causes, err.Error())
 	}
 	return ret
 }
-func NewNotFoundError(message string) *RestErr {
-	return &RestErr{
-		Message: message,
-		Status:  http.StatusInternalServerError,
-		Error:   "not_found",
+func NewNotFoundError(message string) RestErr {
+	return restErr{
+		message: message,
+		status:  http.StatusInternalServerError,
+		error:   "not_found",
 	}
 }
 
@@ -71,10 +102,18 @@ func NewError(msg string) error {
 	return errors.New(msg)
 }
 
-func NewRestErrorFromBytes(bytes []byte) (*RestErr, error) {
+func NewRestErrorFromBytes(bytes []byte) (RestErr, error) {
 	var apiErr RestErr
 	if err := json.Unmarshal(bytes, &apiErr); err != nil {
 		return nil, errors.New("invalid json")
 	}
-	return &apiErr, nil
+	return apiErr, nil
+}
+
+func NewUnauthorizedError(message string) RestErr {
+	return restErr{
+		message: message,
+		status:  http.StatusUnauthorized,
+		error:   "unauthorized",
+	}
 }
